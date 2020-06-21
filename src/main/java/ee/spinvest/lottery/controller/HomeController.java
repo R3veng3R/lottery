ee63d33cfd39dfd19af37f7ed4f7311894e7ad86
Aleksei <evilstormhot@gmail.com>
2020-06-21 10:39:50 +0300

    Fix build Add app version Remove old add-ticket method Add some css styles Introduce fragment for search-result.html

--- src/main/java/ee/spinvest/lottery/controller/HomeController.java
+++ src/main/java/ee/spinvest/lottery/controller/HomeController.java
@@ -2,37 +2,30 @@ package ee.spinvest.lottery.controller;
 
 import ee.spinvest.lottery.model.Ticket;
 import ee.spinvest.lottery.service.TicketService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Date;
 import java.util.List;
 
 @Controller
 public class HomeController {
     private final TicketService ticketService;
 
+    @Value("${app.version}")
+    private String appVersion;
+
     public HomeController(final TicketService ticketService) {
         this.ticketService = ticketService;
     }
 
     @GetMapping("/")
-    public String index(Model model) {
-        List<Ticket> tickets = ticketService.findAll();
+    public String index(final Model model) {
+        final List<Ticket> tickets = ticketService.findAll();
 
-        model.addAttribute("message", "world!");
         model.addAttribute("tickets", tickets);
+        model.addAttribute("appVersion", appVersion);
         return "layout";
-    }
-
-    @PostMapping("/add-ticket")
-    public ModelAndView addTicket(@ModelAttribute("ticket") Ticket ticket) {
-        ticketService.save(ticket);
-        return new ModelAndView("redirect:/");
     }
 }
